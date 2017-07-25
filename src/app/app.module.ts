330a9852801ffe872a27af406cb3b6af501ac1bf
elsonsilva <elsoncarlos@gmail.com>
2017-07-25 09:54:40 -0300

    primeiro commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+import {AppComponent} from './app.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import {DialogComponent} from './dialog/dialog.component';
+
+import 'hammerjs';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DialogComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    FlexLayoutModule,
+    NgxChartsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [
+
+  ],
+  entryComponents: [DialogComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
+
