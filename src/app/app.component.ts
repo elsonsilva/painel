ee0cd4964d95f09044770e78700d0575fb4b0170
elsonsilva <elsoncarlos@gmail.com>
2017-07-31 18:55:03 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,7 @@ export class AppComponent implements OnInit {
   // private categories: number[] = [2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011];
 
   public datadonut: any[] = [{
-    kind: 'Hydroelectric', share: 0.175
+    kind: 'Hydro', share: 0.175
   }, {
     kind: 'Nuclear', share: 0.238
   }, {
